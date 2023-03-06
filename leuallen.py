6b45c90840f80c1d816b2cf38d55ef661cb57350
Katelyn Leuallen <126613996+katelynleuallen@users.noreply.github.com>
2023-03-06 19:39:09 +0000

    Initial connect

--- leuallen.py
+++ leuallen.py
@@ -13,6 +13,19 @@ def print_board(board):
 def move(player, board, score):
   r = 0
   c = 0
+  if board[0][0] == '':
+    r=0
+    c=0
+  elif board[0][2] == '':
+    r=0
+    c=2
+  elif board[2][0] =='':
+    r=2
+    c=0
+ 
+  elif board [2][2] =='':
+    r=2
+    c=2
   while board[r][c] != ' ':
     r = random.randint(0,2)
     c = random.randint(0,2)
