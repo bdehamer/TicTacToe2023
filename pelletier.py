d3f67af66ae85030bc226bf948864dff2bb061f6
KAMRYNN PELLETIER <126613920+kamrynn-ness@users.noreply.github.com>
2023-03-07 18:54:53 +0000

    update

--- pelletier.py
+++ pelletier.py
@@ -8,30 +8,29 @@ def move(player, board, score):
   if board[1][1] == ' ':
     r=1
     c=1
+
   elif board[0][2] ==' ':
     r=0
+    c=2
+  elif board [2][2] ==' ':
+    r=2
     c=2
   elif board [2][0] ==' ':
     r=2
     c=0
-  elif board [2][2] ==' ':
-    r=2
-    c=2
-  elif board[1][2] ==' ':
+  elif board [1][2] == ' ':
     r = 1
     c=2
-  elif board [2][1]==' ':
-    r=2
-    c=1  
- 
-  elif board [1][0] ==' ':
+  elif board [2][1] ==' ':
+    r = 2
+    c=1
+  elif board[1][0] ==' ':
     r = 1
     c=0
-  elif board [0][1] == ' ':
-    r = 0
-    c=1
-
-  elif board[0][0] == ' ':
+  elif board [0][0]==' ':
     r=0
-    c=0     
+    c=0
+  elif board[0][1] == ' ':
+    r=0
+    c=1
   return r,c
