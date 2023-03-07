06668cb2b73efec3c16031fc3d92ed52ef888f49
allison <126613918+allisonbryan@users.noreply.github.com>
2023-03-07 19:37:30 +0000

    small edit

--- bryan.py
+++ bryan.py
@@ -2,13 +2,6 @@ team_name = 'bryan'
 strategy_name = 'just picking spots tbh'
 strategy_description = 'best spots'
 
-'''def print_board(board):
-  print(board[0][0]+'|'+board[0][1]+'|'+board[0][2])
-  print('-+-+-')
-  print(board[1][0]+'|'+board[1][1]+'|'+board[1][2])
-  print('-+-+-')
-  print(board[2][0]+'|'+board[2][1]+'|'+board[2][2])
-'''
 
 def move(player, board, score):
   r = 0
