0f57bc93158b5552018abe13f150c9fa5417590d
Caleb Farmer <126613902+CalebFarmer@users.noreply.github.com>
2023-03-07 19:28:19 +0000

    Forgot to get rid of printing board and player the first time.

--- farmer.py
+++ farmer.py
@@ -1,17 +1,9 @@
 team_name = 'farmer'
 strategy_name = 'farmer strategy'
 strategy_description = 'Take the L'
-def print_board(board):
-  print(board[0][0]+'|'+board[0][1]+'|'+board[0][2])
-  print('-+-+-')
-  print(board[1][0]+'|'+board[1][1]+'|'+board[1][2])
-  print('-+-+-')
-  print(board[2][0]+'|'+board[2][1]+'|'+board[2][2])
+
 
 def move(player, board, score):
-  print(player)
-  print(" ")
-  print_board(board)
   if board[1][1] == ' ':
     c = 1
     r = 1
