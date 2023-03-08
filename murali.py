660831594b6861f5d19e9f9f84562f01fc15d7ae
ROHAN MURALI <126614034+rohmur@users.noreply.github.com>
2023-03-08 19:35:26 +0000

    fixed problem

--- murali.py
+++ murali.py
@@ -1,15 +1,8 @@
 import random
 team_name = 'Murali'
 strategy_name = 'murali strategy'
-strategy_description = 'Play the next open spot.'
+strategy_description = 'take the middle and move up or down and strike through.'
 
-def print_board(board):
-  print(board[0][0]+'|'+board[0][1]+'|'+board[0][2])
-  print('-+-+-')
-  print(board[1][0]+'|'+board[1][1]+'|'+board[1][2])
-  print('-+-+-')
-  print(board[2][0]+'|'+board[2][1]+'|'+board[2][2])
-    
 def move(player, board, score):
   if board[1][1] == ' ':
     r = 1
