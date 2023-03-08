cb348e458730e6992ecb111d68eeccd707827142
Javi <126613868+Javi-Rasc@users.noreply.github.com>
2023-03-08 19:30:03 +0000

    Updated Rascon Strategy + added name and description

--- Rascon.py
+++ Rascon.py
@@ -1,70 +1,40 @@
 import random
 team_name = 'Rascon'
-strategy_name = 'Samuel you a****** you better not steal my strategy'
-strategy_description = 'Blocking + idk'
+strategy_name = 'right wing'
+strategy_description = 'Go for right column then random'
 
-def check_works(board, r, c):
+def place_random(r, c, opp, board, player):
   r = random.randint(0,2)
   c = random.randint(0,2)
-  if board[r][c] != ' ':
+  if board[r][c] != player or opp:
     return r, c
   else:
-    check_works(board, r, c,)
+    place_random(r, c, opp, board, player)
+    
+def print_board(board):
+  print(board[0][0]+'|'+board[0][1]+'|'+board[0][2])
+  print('-+-+-')
+  print(board[1][0]+'|'+board[1][1]+'|'+board[1][2])
+  print('-+-+-')
+  print(board[2][0]+'|'+board[2][1]+'|'+board[2][2])
 
 def move(player, board, score):
-
   r = 0
   c = 0
-
- 
-    #_____Checks_Corners_and_Center_____#
-  if board[1][1] == ' ': #center
-    r = 1
-    c = 1
-  elif board[0][0] == ' ': #top right
-    r = 0
-    c = 0
-  elif board[0][2] == ' ': #top left
+  if board[0][2] == ' ':
     r = 0
     c = 2
-  elif board[2][0] == ' ': #bottom right
-    r = 2
-    c = 0
-  elif board[2][2] == ' ': #bottom left
+  elif board[2][2] == ' ':
     r = 2
     c = 2
-
-    #____Checks_for_possilbe_rows/colums_____#
-  elif board[0][0] and board [2][0] == player or ' ': #mid left
-    r = 1
-    c = 0
-  elif board[0][2] and board [2][2] == player or ' ': #mid right
+  elif board[1][2] == ' ':
     r = 1
     c = 2
-  elif board[0][0] and board [0][2] == player or ' ': #mid top
-    r = 0
-    c = 1
-  elif board[2][0] and board [2][2] == player or ' ': #mid bottom
-    r = 2
-    c = 1
-    
-  #_____Checks_and_Blocks_____#
-  elif board[0][0] and board [2][0] != player: #mid left
-    r = 1
-    c = 0
-  elif board[0][2] and board [2][2] != player: #mid right
-    r = 1
-    c = 2
-  elif board[0][0] and board [0][2] != player: #mid top
-    r = 0
-    c = 1
-  elif board[2][0] and board [2][2] != player: #mid bottom
-    r = 2
-    c = 1
-
   else:
-    check_works(board, r, c)
-  
+    r = random.randint(0,2)
+    c = random.randint(0,2)
+    while board[r][c] != ' ':
+      r = random.randint(0,2)
+      c = random.randint(0,2)
   return r, c
-
 
