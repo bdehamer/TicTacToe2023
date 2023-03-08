f94981efb2319b5f0076f7201a43100319f42a2e
Katelyn Leuallen <126613996+katelynleuallen@users.noreply.github.com>
2023-03-08 01:01:58 +0000

    I changed my move.

--- leuallen.py
+++ leuallen.py
@@ -1,35 +1,31 @@
 import random
 team_name = 'leuallen'
 strategy_name = 'leuallen strategy'
-strategy_description = 'Play the next open spot.'
+strategy_description = 'corner, center, coner?'
 
-def print_board(board):
-  print(board[0][0]+'|'+board[0][1]+'|'+board[0][2])
-  print('-+-+-')
-  print(board[1][0]+'|'+board[1][1]+'|'+board[1][2])
-  print('-+-+-')
-  print(board[2][0]+'|'+board[2][1]+'|'+board[2][2])
-  
+
 def move(player, board, score):
-  r = 0
-  c = 0
-  if board[0][0] == '':
-    r=0
-    c=0
-  elif board[0][2] == '':
-    r=0
-    c=2
-  elif board[2][0] =='':
-    r=2
-    c=0
- 
-  elif board [2][2] =='':
-    r=2
-    c=2
-  while board[r][c] != ' ':
+  if board[1][1] == ' ':
+    r = 1
+    c = 1
+  elif board[0][0] == ' ' and board[2][0] == ' ':
+    r = 0
+    c = 0
+  elif board[0][0] == player and board[2][0] == ' ':
+    r = 2
+    c = 0
+  elif board[2][2] == ' ' and board[0][2] == ' ':
+    r = 0
+    c = 2
+  elif board[2][2] == player and board[0][2] == ' ':
+    r = 2
+    c = 2
+  else:
     r = random.randint(0,2)
     c = random.randint(0,2)
-    
-  print(r,c)
-  print_board(board)
+
+    while board[r][c] != ' ':
+      r = random.randint(0,2)
+      c = random.randint(0,2)
+  
   return r, c
