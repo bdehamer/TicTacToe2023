b214e05bb7e947b3e96db52d82f76aedfc2f0e39
camethomas39415 <126613925+CameronT13@users.noreply.github.com>
2023-03-08 19:45:23 +0000

    deleted print baord statement

--- Thomas.py
+++ Thomas.py
@@ -3,12 +3,7 @@ strategy_name = 'logik'
 strategy_description = 'counteracts conventional logic plays.'
 import random as rand
 
-def print_board(board):
-  print(board[0][0]+'|'+board[0][1]+'|'+board[0][2])
-  print('-+-+-')
-  print(board[1][0]+'|'+board[1][1]+'|'+board[1][2])
-  print('-+-+-')
-  print(board[2][0]+'|'+board[2][1]+'|'+board[2][2])
+
 
 def move(player, board, score):
   r = 0
